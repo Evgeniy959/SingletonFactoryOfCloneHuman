07a4528da3c471b28054d991cc53e7fc718ea908
Pili_zr36 <Pili_zr36@itstepru.onmicrosoft.com>
2022-03-05 22:14:58 +0700

    Add project files.

---
+++ SingletonFactoryOfCloneHuman/FactoryClones.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonFactoryOfCloneHuman
+{
+    public class FactoryClones
+    {
+        private static FactoryClones _instance;
+        private FactoryClones()
+        { }
+
+        public static FactoryClones GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new FactoryClones();
+            }
+            return _instance;
+        }
+
+        public string Say(IClone @interface)
+        {
+            return @interface.Say();
+        }
+    }
+}
