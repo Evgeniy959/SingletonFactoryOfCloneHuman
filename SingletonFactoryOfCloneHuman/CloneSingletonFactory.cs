6f4466c7d606e89869c5668b28f8ef492f205023
Pili_zr36 <Pili_zr36@itstepru.onmicrosoft.com>
2022-03-06 10:14:18 +0700

    Done

--- SingletonFactoryOfCloneHuman/CloneSingletonFactory.cs
+++ SingletonFactoryOfCloneHuman/CloneSingletonFactory.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SingletonFactoryOfCloneHuman
 {
-    public class CloneSingletonFactory //Singletone фабрика
+    public class CloneSingletonFactory //Singletone (одиночка) фабрика
     {
         private static CloneSingletonFactory _singleton;
 
